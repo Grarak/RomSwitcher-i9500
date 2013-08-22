121b5561b5ce97c68c814365f7bfc19997c3e676
Grarak <williye97@gmail.com>
2013-08-22 18:37:29 +0200

    add modules
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/db-bindmount.sh
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -4,10 +4,11 @@ mkdir -p /1stdata/dual/2nddata
 mount --bind /1stdata/dual/2nddata /data
 
 mount -o remount,rw /system
-
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
+mount -t tmpfs tmpfs /system/lib/modules
 
 chmod 755 /system
+ln -s /lib/modules/* /system/lib/modules
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
