0c6f75ed298535e5bd449104a63580dbf671abe6
Grarak <williye97@gmail.com>
2013-08-22 08:30:21 +0200

    CyanogenMod Ramdisk
    Signed-off-by: Grarak <williye97@gmail.com>

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd boot.img-ramdisk
+find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
+cd ..
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img
+
+cp -v boot.img out/
+cd out
+zip -r kernel.zip META-INF boot.img
+mv -v kernel.zip ../
+cd ..
+adb push kernel.zip /sdcard/.
