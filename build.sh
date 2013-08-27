96c6ae4a756f87260f9af731c5e68cb50d68e80d
Grarak <williye97@gmail.com>
2013-08-27 17:29:46 +0200

    support for darwin
    Signed-off-by: Grarak <williye97@gmail.com>

--- build.sh
+++ build.sh
@@ -4,10 +4,18 @@ rm -rf kernel.zip
 rm -rf ramdisk.gz
 find -name "*~" -exec rm -rf {} \;
 
-cd boot.img-ramdisk
-find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
-cd ..
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img
+build () {
+    cd boot.img-ramdisk
+    find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
+    cd ..
+    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz -o boot.img
+}
+
+if grep -q "export BUILD_MAC_SDK_EXPERIMENTAL=1" ~/.bash_profile; then
+    build darwin
+else
+    build linux
+fi
 
 mv -v boot.img out/
 cd out
