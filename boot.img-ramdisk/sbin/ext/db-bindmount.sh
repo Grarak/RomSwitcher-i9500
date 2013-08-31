9631052ee14e3835f0ae6e17e806e3b635bb4062
Grarak <williye97@gmail.com>
2013-09-01 00:55:53 +0200

    res: remove RomSwitcher.apk
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/db-bindmount.sh
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /1stdata/dual/2nddata
 mount --bind /1stdata/dual/2nddata /data
-mount --bind /1stdata/app /data/app
+#mount --bind /1stdata/app /data/app
 
 mount -o remount,rw /system
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
@@ -11,9 +11,6 @@ mount -t tmpfs tmpfs /system/lib/modules
 chmod 771 /1stdata
 chmod 755 /system
 ln -s /lib/modules/* /system/lib/modules/
-
-mv -f /res/app/* /system/app/
-chmod 0644 /system/app/*.apk
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
