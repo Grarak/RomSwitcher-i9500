11ad10d63610622dc43044dcaa545fa602baaa1d
Grarak <williye97@gmail.com>
2013-09-28 23:03:34 +0200

    share romswitcher data
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/db-bindmount.sh
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -2,18 +2,19 @@
 
 mkdir -p /.firstrom/media/.secondrom/data
 mount --bind /.firstrom/media/.secondrom/data /data
-#mount --bind /.firstrom/media/.secondrom/data /data/app
 
 mount -o remount,rw /system
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
 mount -t tmpfs tmpfs /system/lib/modules
 
-chmod 771 /1stdata
+chmod 771 /.firstrom
 chmod 755 /system
 ln -s /lib/modules/* /system/lib/modules/
 
 cp -f /.firstrom/app/com.grarak.*.apk /.firstrom/media/.secondrom/data/app/
 chmod 755 /.firstrom/media/.secondrom/data/app/*.apk
+
+mount --bind /.firstrom/data/com.grarak.romswitcher /data/data/com.grarak.romswitcher
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
