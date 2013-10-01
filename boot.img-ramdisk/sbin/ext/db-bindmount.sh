8081a6f6960bb21e774a8f8c25ad5baab7c05010
Willi Ye <williye97@gmail.com>
2013-10-01 11:20:00 +0200

    fix derp

--- boot.img-ramdisk/sbin/ext/db-bindmount.sh
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -20,14 +20,14 @@ chmod 755 /.firstrom/media/.secondrom/data/app/*.apk
 
 if [ -e /romswitcher/appshare ]; then
     mkdir -p /data/app
+    mkdir -p /data/app-asec
+    mkdir -p /data/misc/systemkeys
     mount --bind /.firstrom/app /data/app
+    mount --bind /.firstrom/data /data/data
+    mount --bind /.firstrom/app-asec /data/app-asec
     if [ -e /romswitcher/datashare ]; then
         rm -rf /.firstrom/data/com.android.providers.telephony
         mkdir -p /data/data
-        mkdir -p /data/app-asec
-        mkdir -p /data/misc/systemkeys
-        mount --bind /.firstrom/data /data/data
-        mount --bind /.firstrom/app-asec /data/app-asec
         mount --bind /.firstrom/misc/systemkeys /data/misc/systemkeys
     fi
 fi
