b601897c5904bcde2e679e79ccab55359c1f67f4
Grarak <williye97@gmail.com>
2013-10-06 20:17:03 +0200

    fix derp
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/mount_system.sh
+++ boot.img-ramdisk/sbin/ext/mount_system.sh
@@ -3,4 +3,4 @@
 mkdir -p /system
 mkdir -p /.firstrom/media/.secondrom
 losetup /dev/block/loop0 /.firstrom/media/.secondrom/system.img
-mount -t ext4 -o ro /.secondrom/media/.secondrom/system.img /system
+mount -t ext4 -o ro /firstrom/media/.secondrom/system.img /system
