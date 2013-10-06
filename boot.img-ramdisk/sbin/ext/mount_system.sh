e740431f49589528994d69cc47ddd3335ad9822b
Grarak <williye97@gmail.com>
2013-10-06 21:28:20 +0200

    fix boot up
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/mount_system.sh
+++ boot.img-ramdisk/sbin/ext/mount_system.sh
@@ -1,9 +1,5 @@
 #!/sbin/busybox sh
 
 mkdir -p /system
-mkdir -p /.firstrom
-mount -t ext4 -o rw /dev/block/mmcblk0p21 /.firstrom
-mkdir -p /.firstrom/media/.secondrom
 losetup /dev/block/loop0 /.firstrom/media/.secondrom/system.img
 mount -t ext4 -o ro /.firstrom/media/.secondrom/system.img /system
-umount -f /.firstrom
