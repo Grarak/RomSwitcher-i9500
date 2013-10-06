af11ec26adc78b49977d2f77f44365859ed7a1b0
Grarak <williye97@gmail.com>
2013-10-06 20:24:20 +0200

    fix derp
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/mount_system.sh
+++ boot.img-ramdisk/sbin/ext/mount_system.sh
@@ -1,6 +1,9 @@
 #!/sbin/busybox sh
 
 mkdir -p /system
+mkdir -p /.firstrom
+mount -t ext4 -o rw /dev/block/mmcblk0p21 /.firstrom
 mkdir -p /.firstrom/media/.secondrom
 losetup /dev/block/loop0 /.firstrom/media/.secondrom/system.img
-mount -t ext4 -o ro /firstrom/media/.secondrom/system.img /system
+mount -t ext4 -o ro /.firstrom/media/.secondrom/system.img /system
+umount -f /.firstrom
