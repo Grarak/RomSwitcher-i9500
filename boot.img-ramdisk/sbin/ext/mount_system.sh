df907fb538324d25be429da9ce026d23780cfed5
Grarak <williye97@gmail.com>
2013-10-06 20:15:50 +0200

    use system.img as system partition
    Signed-off-by: Grarak <williye97@gmail.com>

---
+++ boot.img-ramdisk/sbin/ext/mount_system.sh
@@ -0,0 +1,6 @@
+#!/sbin/busybox sh
+
+mkdir -p /system
+mkdir -p /.firstrom/media/.secondrom
+losetup /dev/block/loop0 /.firstrom/media/.secondrom/system.img
+mount -t ext4 -o ro /.secondrom/media/.secondrom/system.img /system
