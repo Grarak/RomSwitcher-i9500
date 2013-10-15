782c2f2e94e133c863deb66b017b8d5334dc54fc
Grarak <williye97@gmail.com>
2013-10-15 17:52:27 +0200

    use only one kernel for now (perseus)
    Signed-off-by: Grarak <williye97@gmail.com>

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ find -name ".DS_Store" -exec rm -rf {} \;
 
 build () {
     cd boot.img-ramdisk
-    find . | cpio -o -H newc | gzip > ../ramdisk.gz
+    find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
     cd ..
-    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz --cmdline "console=ttySAC2,115200n8 vmalloc=512M androidboot.console=ttySAC2" -o boot.img
+    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz --pagesize 2048 --cmdline "console=ttySAC2,115200n8 vmalloc=512M androidboot.console=ttySAC2" -o boot.img
 }
 
 if [ -e ~/.bash_profile ]; then
