0d938fcddaf608682e74a8a5ce7ed395ed128bf7
Grarak <williye97@gmail.com>
2013-10-31 16:57:44 +0100

    update perseus kernel
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/perseus.sh
+++ boot.img-ramdisk/sbin/ext/perseus.sh
@@ -23,12 +23,15 @@ echo 480 > /sys/devices/platform/pvrsrvkm.0/sgx_dvfs_max_lock
 echo 50 > /sys/class/devfreq/exynos5-busfreq-mif/polling_interval
 echo 70 > /sys/class/devfreq/exynos5-busfreq-mif/time_in_state/upthreshold
 
+/sbin/ext/uci
+
 mkdir -p /mnt/ntfs
 chmod 777 /mnt/ntfs
 mount -o mode=0777,gid=1000 -t tmpfs tmpfs /mnt/ntfs
 
-ln -s /res/synapse/uci /sbin/uci
-/sbin/uci
+if [ -d /system/etc/init.d ]; then
+	/sbin/busybox run-parts /system/etc/init.d
+fi;
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
 mount -o remount,ro /system
