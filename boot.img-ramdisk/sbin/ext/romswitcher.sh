f669ac107b9ce21ab835cf20520f53674c6a76d9
Willi Ye <williye97@gmail.com>
2014-06-30 00:27:52 +0200

    update recovery and ramdisk
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/romswitcher.sh
+++ boot.img-ramdisk/sbin/ext/romswitcher.sh
@@ -10,11 +10,22 @@ mkdir -p /.firstrom/media/.${ROM}rom/data/app
 cp -f /.firstrom/app/com.grarak.*.apk /.firstrom/media/.${ROM}rom/data/app/
 chmod 755 /.firstrom/media/.${ROM}rom/data/app/*.apk
 
-if [ "$ROM" == "2" ] && [ -e /romswitcher/appshare ]; then
-	mkdir -p /data/app
-	mkdir -p /data/app-asec
-	mkdir -p /data/misc/systemkeys
-	mount --bind /.firstrom/app /data/app
-	mount --bind /.firstrom/app-asec /data/app-asec
-	mount --bind /.firstrom/misc/systemkeys /data/misc/systemkeys
+if [ "$ROM" != "1" ] && [ -f /romswitcher/appsharing ]; then
+	echo "" >> /romswitcher/appsharing
+	while read ROM1 ROM2; do
+		if [ "$ROM2" == "$ROM" ]; then
+			mkdir -p /data/app
+			mkdir -p /data/app-asec
+			mkdir -p /data/misc/systemkeys
+			if [ "$ROM1" == "1" ]; then
+				mount --bind /.firstrom/app /data/app
+				mount --bind /.firstrom/app-asec /data/app-asec
+				mount --bind /.firstrom/misc/systemkeys /data/misc/systemkeys
+			else
+				mount --bind /data/media/.${ROM1}rom/data/app /data/app
+				mount --bind /data/media/.${ROM1}rom/data/app-asec /data/app-asec
+				mount --bind /data/media/.${ROM1}rom/data/misc/systemkeys /data/misc/systemkeys
+			fi
+		fi
+	done < /romswitcher/appsharing
 fi
